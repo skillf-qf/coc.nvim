6419e1f7a86a8c1ccae98322618e911b46f8f0c1
Heyward Fann <fannheyward@gmail.com>
2020-12-25 10:36:30 +0800

    feat(semanticTokens): update, but still cant work
    TODO: SemantiTokens

--- src/language-client/index.ts
+++ src/language-client/index.ts
@@ -471,6 +471,7 @@ export class LanguageClient extends BaseLanguageClient {
     this.registerFeature(new SelectionRangeFeature(this))
     this.registerFeature(new ProgressFeature(this))
     this.registerFeature(new CallHierarchyFeature(this))
+    // TODO: SemantiTokens
     // this.registerFeature(new SemanticTokensFeature(this))
     if (!this.clientOptions.disableWorkspaceFolders) {
       this.registerFeature(new WorkspaceFoldersFeature(this))
